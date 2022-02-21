1aa88574cdd77eb89b70129d7397256812bb25a7
Milaen13 <liudmila.engberg@gmail.com>
2022-02-21 11:14:45 +0100

    первый commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        calc.println.accept(a);
+        int b = calc.minus.apply(1, 1);
+        calc.println.accept(b);
+        double c = calc.divide.apply(Integer.valueOf(2).doubleValue(), Integer.valueOf(0).doubleValue());
+        calc.println.accept(c);
+        int d = calc.multiply.apply(1, 2);
+        calc.println.accept(d);
+        int e = calc.pow.apply(2);
+        calc.println.accept(e);
+        int f = calc.abs.apply(2);
+        calc.println.accept(f);
+        boolean g = calc.isPositive.test(-2);
+        calc.println.accept(g);
+    }
+}
+
+// нельзя делить на 0
+// BinaryOperator<Double> divide вместо BinaryOperator<Integer> divide
+// меняем Consumer<Integer> println на Consumer<Object> println
